3c6f733994ee750a8011341e97663e667f2eb5b6
KOYILASYSTEMS <koyilasystems@gmail.com>
2024-10-02 23:49:56 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://fooddelivery-backend-8hf6.onrender.com'
+export const url = 'https://fooddelivery-backend-nv02.onrender.com'
