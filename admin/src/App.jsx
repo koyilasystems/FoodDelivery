42ec5ce2513839fcafa5d768b776d7b2c2e1b4cd
KOYILASYSTEMS <koyilasystems@gmail.com>
2024-10-02 22:45:43 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -11,7 +11,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://fooddelivery-backend-8hf6.onrender.com"
 
   return (
     <div>
